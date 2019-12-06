630297e3a9bc071d2c56f0492179775f1fff8f4c
sanmu <sanmu@syedu.com>
2019-12-06 09:11:19 +0800

    FEAT: remove native scroll

--- packages/nativeScroll/index.js
+++
@@ -1,8 +0,0 @@
-import NativeScroll from './nativeScroll'
-
-export default {
-  install (Vue) {
-    // Vue.component(NativeScroll.name, NativeScroll)
-    Vue.use(NativeScroll)
-  }
-}
--- packages/nativeScroll/nativeScroll.js
+++
@@ -1,7 +0,0 @@
-import NativeScroll from './src/NativeScroll'
-
-NativeScroll.install = function (Vue) {
-  Vue.component(NativeScroll.name, NativeScroll)
-}
-
-export default NativeScroll
